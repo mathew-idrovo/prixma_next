6c352c3ac74e55293281b678b7cd66452abd9187
Mathew <mateoicecua2000@gmail.com>
2025-02-25 10:00:39 -0500

    first commit

---
+++ src/app/api/todos/route.ts
@@ -0,0 +1,52 @@
+import prisma from '@/lib/prisma'
+import { NextResponse, NextRequest } from 'next/server'
+import * as yup from 'yup'
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url)
+  const take = Number(searchParams.get('take') ?? '10')
+  const skip = Number(searchParams.get('skip') ?? '10')
+
+  if (isNaN(take)) {
+    return NextResponse.json(
+      { message: 'Take tiene que ser un número' },
+      { status: 400 }
+    )
+  }
+
+  if (isNaN(skip)) {
+    return NextResponse.json(
+      { message: 'Skip tiene que ser un número' },
+      { status: 400 }
+    )
+  }
+
+  const todos = await prisma.todo.findMany({ take, skip })
+  return NextResponse.json(todos)
+}
+
+const postSchema = yup.object({
+  description: yup.string().required(),
+  complete: yup.boolean().optional().default(false),
+})
+
+export async function POST(request: NextRequest) {
+  try {
+    const { description, complete } = await postSchema.validate(
+      await request.json()
+    )
+    const todo = await prisma.todo.create({ data: { description, complete } })
+    return NextResponse.json(todo)
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 400 })
+  }
+}
+
+export async function DELETE(request: NextRequest) {
+  try {
+    await prisma.todo.deleteMany({ where: { complete: true } })
+    return NextResponse.json('Todos completados eliminados')
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 400 })
+  }
+}
